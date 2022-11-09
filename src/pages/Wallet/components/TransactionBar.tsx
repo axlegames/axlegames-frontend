48a7e8bf8f3e473680450e1e08bbd639c070348e
akashmadduru <akashmadduru@gmail.com>
2022-11-09 19:43:11 +0530

    [fix] metamask fix

--- src/pages/Wallet/components/TransactionBar.tsx
+++ src/pages/Wallet/components/TransactionBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import { theme } from "../../../config/theme.config";
 
 import GameBarTile from "./TransactionBarTile";
